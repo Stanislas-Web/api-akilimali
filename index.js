3bfaeb6340d38a7eb40907b04fdfe41b32ac165f
Stanislas-Web <stamislasmakengo1@gmail.com>
2023-10-02 09:18:02 +0100

    update code index

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const http = require("http");
-const app = require("./app");
+// const http = require("http");
+// const app = require("./app");
+// const chalk = require("chalk");
+// const port = process.env.PORT || 4600;
+
+// require('dotenv').config();
+// const server = http.createServer(app);
+
+// const db = require("./models");
+
+// db.sequelize.sync().then(() => {
+//   server.listen(port, () => {
+//     let host = `http://localhost:${port}`;
+//     console.log(chalk.yellow(host));
+//   });
+// });
+
+
+
+
+require('dotenv/config')
+const app = require('./app');
 const chalk = require("chalk");
-const port = process.env.PORT || 4600;
 
-require('dotenv').config();
-const server = http.createServer(app);
 
-const db = require("./models");
+const port = process.env.PORT || 8000;
 
-db.sequelize.sync().then(() => {
-  server.listen(port, () => {
-    let host = `http://localhost:${port}`;
-    console.log(chalk.yellow(host));
-  });
+app.listen(port, () => {
+  let host = `http://localhost:${port}`;
+  console.log('Express server démarré sur le port ' + port + chalk.yellow(host));
 });
+  
