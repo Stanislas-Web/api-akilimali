d178c5ff8e4e39a734ac8d8ffd390e9dfecc4250
Stanislas-Web <stamislasmakengo1@gmail.com>
2023-10-02 15:45:50 +0100

    upload for the server

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
-// const http = require("http");
-// const app = require("./app");
-// const chalk = require("chalk");
-// const port = process.env.PORT || 4600;
+const http = require("http");
+const app = require("./app");
+const chalk = require("chalk");
+const port = process.env.PORT || 4600;
 
-// require('dotenv').config();
-// const server = http.createServer(app);
+require('dotenv').config();
+const server = http.createServer(app);
 
-// const db = require("./models");
+const db = require("./models");
 
-// db.sequelize.sync().then(() => {
-//   app.listen(port, () => {
-//     let host = `http://localhost:${port}`;
-//     console.log(chalk.yellow(host));
-//   });
-// });
-
-
-
-
-require('dotenv/config')
-const app = require('./app');
-// const chalk = require("chalk");
-// const db = require("./models");
-
-
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => {
-  let host = `http://localhost:${port}`;
-  console.log('Express server démarré sur le port ' + port);
+db.sequelize.sync().then(() => {
+  app.listen(port, () => {
+    let host = `http://localhost:${port}`;
+    console.log(chalk.yellow(host));
+  });
 });
 
-// db.sequelize.sync().then(() => {
-  // app.listen(port, () => {
-  //   let host = `http://localhost:${port}`;
-  //   // console.log(chalk.yellow(host));
-  // });
-// });
